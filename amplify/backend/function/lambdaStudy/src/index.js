cc80a57c450b216a72d11a19c95cdd82c42a30c6
sergio <sfilhu@gmai.com>
2021-01-12 16:06:24 -0300

    delete project

--- amplify/backend/function/lambdaStudy/src/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    console.log(event)
-    // TODO implement
-    const response = await {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return JSON.stringify(response);
-};
